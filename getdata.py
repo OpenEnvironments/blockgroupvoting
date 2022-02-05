19373861aa1f46886340506b820f13c4837fddd0
michaelbbryan <michael.b.bryan@gmail.om>
2022-02-05 17:29:00 -0500

    Updated getdata.py for 2 sources and census sample.

--- getdata.py
+++ getdata.py
@@ -1,6 +1,42 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 """
-Download the tigerline files by state.
+
+These functions download and unzip the state based files needed for blockgroupvoting allocations.
 
 https://www2.census.gov/geo/tiger/TIGER2021/BG/    get the file lists
 
 """
+import wget
+from zipfile import ZipFile
+
+def get_blockgroups():
+    """
+    Downloads the state based files that have shapes for Census block groups
+    :return: null
+    """
+    statefips = ['01','02','04','05','06','08','09','10','11','12','13', \
+                 '15','16','17','18','19','20','21','22','23','24','25', \
+                 '26','27','28','29','30','31','32','33','34','35','36', \
+                 '37','38','39','40','41','42','44','45','46','47','48', \
+                 '49','50','51','53','54','55','56','60','66','69','72','78']
+
+    datapath = "D:/Open Environments/data/census/tiger/getdata/"
+
+    for s in statefips:
+        fn = 'tl_2021_' + s + '_bg.zip'
+        url = 'https://www2.census.gov/geo/tiger/TIGER2021/BG/' + fn
+        wget.download(url, out=datapath)
+        with ZipFile(datapath + fn, 'r') as Z:
+           Z.extractall(datapath + fn[:-4])
+
+def get_electionresults():
+    """
+    Download the 2020 dataset from here, unzip its main file then unzip state files
+    https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/K7760H
+    """
+
+def get_ACS5YR():
+    """
+    D:\Open Environments\data\census\tiger\ACS_2019_5YR_BG\ACS_2019_5YR_BG.gdb    
+    """"""
--- statesummary.py
+++ statesummary.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This routine reads the election results and block group shape files for each state,
-then summarizes voting by party and
-calculates average votes per precinct.
+then summarizes voting by party with counts for precincts and block groups.
 
 This serves to test data integrity and confirm that precincts have consistent scope.
 
@@ -12,8 +11,6 @@ The output of this report is available at the end of the README.md
 
 import pandas as pd
 import geopandas as gpd
-import warnings
-warnings.filterwarnings("error")
 
 # Get the list of state abbreviations
 
@@ -29,14 +26,14 @@ Ototal = 0
 Ptotal = 0
 Btotal = 0
 
-print("State\tRepublican\tDemocrat\tLibertarian\tOther\tPrecincts\tVotes per Precinct\tBlock Groups")
-for state in states["Abbreviation"]:
+print("State\tRepublican\tDemocrat\tLibertarian\tOther\tPrecincts\tBlock Groups")
+for i, state in states[~states.Abbreviation.isin(["KY","SD","WV"])].iterrows():
     try:
         state_election = gpd.read_file( \
-            "D:/Open Environments/data/electionscience/dataverse_files/" \
-            + state.lower() + "_2020/" + state.lower() + "_2020.shp")
+            datapath + "electionscience/dataverse_files/" \
+            + state["Abbreviation"].lower() + "_2020/" + state["Abbreviation"].lower() + "_2020.shp")
         blockgroups = gpd.read_file( \
-            datapath + "census/tiger/blockgroups/tl_2021_" + state.lower() + "_bg")
+            datapath + "census/tiger/blockgroups/tl_2021_" + "{:02d}".format(state["StateFIPS"]) + "_bg")
         # Get the presidential race column names for Republican, Democrate & Other
         R = [c for c in state_election.columns.to_list() if c[0:7] == "G20PRER"][0]
         D = [c for c in state_election.columns.to_list() if c[0:7] == "G20PRED"][0]
@@ -49,18 +46,14 @@ for state in states["Abbreviation"]:
         Ptotal += state_election.shape[0]
         Btotal += blockgroups.shape[0]
         # state_election.plot()  # generates a map of precincts within states
-        print(state, "\t",  # State abbreviation
+        print(state["Abbreviation"], "\t",  # State abbreviation
               "{:,}".format(state_election[R].sum().sum()), "\t",  # Repoublican votes
               "{:,}".format(state_election[D].sum().sum()), "\t",  # Democrat votes
               "{:,}".format(state_election[L].sum().sum()), "\t",  # Libertarian votes
               "{:,}".format(state_election[O].sum().sum()), "\t",  # Presidential candidates in other parties
-              "{:,}".format(state_election.shape[0]), "\t",  # number of precincts
-              "{:,.2f}".format( \
-                  (state_election[R].sum().sum() + state_election[D].sum().sum() +
-                   state_election[L].sum().sum() + state_election[O].sum().sum()) / state_election.shape[0]),
-              "{:,}".format(blockgroups.shape[0])  # number of precincts
+              "{:,}".format(state_election.shape[0]), "\t",        # number of precincts
+              "{:,}".format(blockgroups.shape[0])                  # number of block groups
               )  # print statement
-
     except:
         missing = missing.append(states[states.Abbreviation == state])
 
@@ -70,7 +63,6 @@ print("Total\t", \
       "{:,}".format(Ltotal), "\t", \
       "{:,}".format(Ototal), "\t", \
       "{:,}".format(Ptotal), "\t", \
-      "{:,.2f}".format((Rtotal + Dtotal + Ltotal + Ototal) / Ptotal), "\t", \
       "{:,}".format(Btotal) \
       )
 print("\n")
