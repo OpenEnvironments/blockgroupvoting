f7979c0468aa71585250e36f4d67f6126e50ec08
michaelbbryan <michael.b.bryan@gmail.com>
2022-04-04 15:25:46 -0400

    Cleanup for parallel processing

--- parallel.sh
+++ parallel.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+# Shell script to run blockgroup voting in parallel across states
+# -t option is the number of this parallel thread
+# -o option is 'out of' how many threads
+# These will depend on the number of CPU threads available
+# and assume at least one of the CPU threads is needed by OS background tasks
+
 python3 parallelprocess.py -t 0 -o 4 > zero.log  2>zero.err  &
 python3 parallelprocess.py -t 1 -o 4 > one.log   2>one.err   & 
 python3 parallelprocess.py -t 2 -o 4 > two.log   2>two.err   & 
